b5e7c0dbcc4df296ba26db952e87e16211be824a
ektarathour <76484381+ektarathour@users.noreply.github.com>
2020-12-23 09:15:57 +0530

    Add files via upload

---
+++ AVG4.C
@@ -0,0 +1,16 @@
+
+ #include<stdio.h>
+ #include<conio.h>
+ void main()
+ {
+ int num1,num2,num3,num4;
+ float avg;
+ clrscr();
+ printf("enter 4 number");
+ scanf("%d%d%d%d",&num1,&num2,&num3,num4);
+avg=(float)(num1+num2+num3+num4)/4;
+ printf("\n avg of %d and %d is=%.2f",avg);
+ getch();
+ }
+
+
---
+++ LEAPYEAR.C
@@ -0,0 +1,17 @@
+
+ #include<stdio.h>
+ #include<conio.h>
+ void main()
+ {
+ int y ;
+ clrscr();
+ printf("enter year");
+ scanf("%d",&y);
+if(4==0)
+ printf("\n year is leap year");
+ else
+ printf("\n year is not  year");
+  getch();
+ }
+
+

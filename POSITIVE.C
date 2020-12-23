1a4467226e21268b521737d3805ad7eb374e7b2e
ektarathour <76484381+ektarathour@users.noreply.github.com>
2020-12-23 09:21:22 +0530

    Add files via upload

---
+++ POSITIVE.C
@@ -0,0 +1,17 @@
+
+ #include<stdio.h>
+ #include<conio.h>
+ void main()
+ {
+int num;
+clrscr();
+printf("enter a number");
+scanf("%d",&num);
+if(num>=0 )
+printf("\n  num is positive");
+else
+printf("\n num is not positive");
+ getch();
+ }
+
+

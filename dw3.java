94937c7e03c590c35292d8aa410f32ed0f8b0fd2
ektarathour <76484381+ektarathour@users.noreply.github.com>
2021-01-19 19:22:18 +0530

    Add files via upload

---
+++ dw3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class dw3
+{
+public static void main(String[] args)
+{
+int i=1;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the value of n");
+int n=sc.nextInt();
+System.out.println("number are");
+do
+{
+	System.out.println(i);
+i++;
+}
+while(i<=n);
+}
+}

465f8b8eaefa1a5ffd278b3089dfb663091c0084
ektarathour <76484381+ektarathour@users.noreply.github.com>
2021-01-20 12:13:06 +0530

    Add files via upload

---
+++ w3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class w3
+{
+public static void main(String[] args)
+{
+int i=1;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the value of n");
+int n=sc.nextInt();
+System.out.println("number are");
+while(i<=n)
+{
+System.out.println(i);
+i++;
+}
+}
+}
